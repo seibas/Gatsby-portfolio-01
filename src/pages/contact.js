c4cba774e606f951ad17f02b848eaa5a9a87fe6d
sebastiano <36180003+seibas@users.noreply.github.com>
2021-08-24 14:45:10 +0900

    contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,14 +1,11 @@
 import React from "react"
-import Layout from "../components/Layout"
+import contactStyles from "../pages/contact.module.css"
 import styled from "styled-components"
 
+
 const LayoutContacts = styled.div`
-  background: rgb(255, 133, 65);
-  background: radial-gradient(
-    circle,
-    rgba(255, 133, 65, 1) 0%,
-    rgba(66, 66, 66, 0.8014529885063941) 100%
-  );
+ background: rgb(227,227,227);
+background: radial-gradient(circle, rgba(227,227,227,1) 0%, rgba(238,237,237,0.8014529885063941) 100%);
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -21,10 +18,11 @@ function contact() {
   return (
     <LayoutContacts>
       <h1>contact page</h1> <h1>Thank you for contacting</h1>
-      <form
+      <form 
         style={{
           display: "flex",
           flexDirection: "column",
+       
           width: "30rem",
           margin: "auto",
           padding: "1rem",
@@ -38,7 +36,7 @@ function contact() {
         <input name="name" placeholder="Your Name" type="text" />
         <p>
           <label>
-            Your Email: <input type="email" name="email" />
+             <input className= {'mail'} type="email"  placeholder="Your Email" name="email" />
           </label>
         </p>
         <textarea name="message" id="" cols="30" rows="10"></textarea>
